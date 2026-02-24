791e1d97c82469f008371f3393b65235a001ad51
Shefkay <sherifayantoyinbo@gmail.com>
2026-02-24 14:11:53 +0100

    add: create Essential page and implement DAppCard component

---
+++ src/components/features/DAppCard.jsx
@@ -0,0 +1,12 @@
+
+
+function DAppCard() {
+
+  return (
+    <div>
+      <div className="p-3 bg-primary">DAppCard</div>
+    </div>
+  )
+}
+
+export default DAppCard;
--- src/pages/Essential.jsx
+++ src/pages/Essential.jsx
@@ -1,9 +1,23 @@
+import DAppCard from "../components/features/DAppCard";
+
 function Essential() {
+
   return (
     <section className="relative min-h-screen items-center bg-background text-white">
       <div className="container mx-auto px-6 pt-32 pb-20 relative">
-        <div>
-          essential
+        <div className="space-y-4 text-center">
+          <h1 className="text-7xl">Essential Apps (Beta)</h1>
+          <p>Make your homescreen yours - build apps and browse community creations.</p>
+        </div>
+        <div className="grid grid-cols-4 gap-3 py-32">
+          <DAppCard />
+          <DAppCard />
+          <DAppCard />
+          <DAppCard />
+          <DAppCard />
+          <DAppCard />
+          <DAppCard />
+          <DAppCard />
         </div>
       </div>
     </section>
